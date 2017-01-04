60911abd19ff94484866f7f2fd1eb8ade0c7843d
oh geun taek <rmsxor94@naver.com>
2017-01-04 17:44:19 +0900

    [임시저장] 사진 회전 기능을 구현하였으나 사진 불러오는 작업에서는 적용 안됨

--- GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/MainActivity.java
+++ GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/MainActivity.java
@@ -2,6 +2,8 @@ package com.taek_aaa.goalsnowball;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -11,6 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,7 +46,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        imageButton = (ImageButton)findViewById(R.id.mainImageView);
+        imageButton = (ImageButton) findViewById(R.id.mainImageView);
         imageButton.setBackgroundResource(R.drawable.addpicture32);
 
 
@@ -107,7 +110,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void onClickMainImage(View v){
+    public void onClickMainImage(View v) {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -120,19 +123,28 @@ public class MainActivity extends AppCompatActivity
             case PICK_FROM_ALBUM:
                 try {
                     Uri uri = data.getData();
+                    String uriPath = uri.getPath();
+                    String dataPath = data.getDataString();
+                    Log.e("test", uriPath);
+                    Log.e("test2", dataPath);
                     photo = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                     inflater = getLayoutInflater();
+                    Bitmap rotatedPhoto;
+
+                    ExifInterface exif = new ExifInterface(uriPath);
+                    int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int exifDegree = exifOrientationToDegrees(exifOrientation);
+                    rotatedPhoto = rotate(photo, exifDegree);
 
                     float width = photo.getWidth();
                     float height = photo.getHeight();
-                    if(height>viewHeight){
-                        float percente = height/100;
-                        float scale = viewHeight/percente;
-                        width*=scale/100;
-                        height*=scale/100;
+                    if (height > viewHeight) {
+                        float percente = height / 100;
+                        float scale = viewHeight / percente;
+                        width *= scale / 100;
+                        height *= scale / 100;
                     }
-                    Bitmap sizedPhoto = Bitmap.createScaledBitmap(photo,(int)width,(int)height,true);
-
+                    Bitmap sizedPhoto = Bitmap.createScaledBitmap(rotatedPhoto, (int) width, (int) height, true);
 
 
                     imageButton = (ImageButton) findViewById(R.id.mainImageView);
@@ -145,4 +157,40 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    public int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    public Bitmap rotate(Bitmap bitmap, int degrees)
+    {
+        if(degrees != 0 && bitmap != null)
+        {
+            Matrix m = new Matrix();
+            m.setRotate(degrees, (float) bitmap.getWidth() / 2,
+                    (float) bitmap.getHeight() / 2);
+
+            try
+            {
+                Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0,
+                        bitmap.getWidth(), bitmap.getHeight(), m, true);
+                if(bitmap != converted)
+                {
+                    bitmap.recycle();
+                    bitmap = converted;
+                }
+            }
+            catch(OutOfMemoryError ex)
+            {
+                // 메모리가 부족하여 회전을 시키지 못할 경우 그냥 원본을 반환합니다.
+            }
+        }
+        return bitmap;
+    }
 }
