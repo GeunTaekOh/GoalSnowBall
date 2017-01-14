2ca46edec6af94a630c7a5e5e245d74d6e80ca24
oh geun taek <rmsxor94@naver.com>
2017-01-14 18:46:12 +0900

    [기능구현] 오늘의 목표의 여러 데이터값을 객체의 저장

--- GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/GoalDataSet.java
+++ GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/GoalDataSet.java
@@ -8,9 +8,21 @@ public class GoalDataSet {
     private  String todayGoal;
     private  String weekGoal;
     private  String monthGoal;
-    boolean isTodayGoal = false;
-    boolean isWeekGoal = false;
-    boolean isMonthGoal = false;
+    public boolean isTodayGoal = false;
+    public boolean isWeekGoal = false;
+    public boolean isMonthGoal = false;
+    private String typeToday;
+    private int amountToday;
+    private int unitToday;
+
+    private String typeWeek;
+    private int amountWeek;
+    private int unitWeek;
+
+    private String typeMonth;
+    private int amountMonth;
+    private int unitMonth;
+
 
     public void setTodayGoal(String str){
         this.todayGoal=str;
@@ -24,6 +36,37 @@ public class GoalDataSet {
         this.monthGoal=str;
         this.isMonthGoal=true;
     }
+
+    public void setTypeToday(String ty){
+        this.typeToday=ty;
+    }
+    public void setAmountToday(int am){
+        this.amountToday=am;
+    }
+    public void setUnitToday(int un){
+        this.unitToday=un;
+    }
+
+    public void setTypeWeek(String ty){
+        this.typeWeek=ty;
+    }
+    public void setAmountWeek(int am){
+        this.amountWeek=am;
+    }
+    public void setUnitWeek(int un){
+        this.unitWeek=un;
+    }
+
+    public void setTypeMonth(String ty){
+        this.typeMonth=ty;
+    }
+    public void setAmountMonth(int am){
+        this.amountMonth=am;
+    }
+    public void setUnitMonth(int un){
+        this.unitMonth=un;
+    }
+
     public String getTodayGoal(){
         return this.todayGoal;
     }
@@ -34,4 +77,15 @@ public class GoalDataSet {
         return this.monthGoal;
     }
 
+    public String getTypeToday(){return this.typeToday;}
+    public int getAmountToday(){return this.amountToday;}
+    public int getUnitToday(){return this.unitToday;}
+
+    public String getTypeWeek(){return this.typeWeek;}
+    public int getAmountWeek(){return this.amountWeek;}
+    public int getUnitWeek(){return this.unitWeek;}
+
+    public String getTypeMonth(){return this.typeMonth;}
+    public int getAmountMonth(){return this.amountMonth;}
+    public int getUnitMonth(){return this.unitMonth;}
 }
--- GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/TodayGoalDialog.java
+++ GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/TodayGoalDialog.java
@@ -4,7 +4,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import static com.taek_aaa.goalsnowball.MainActivity.goalDataSet;
@@ -14,19 +19,63 @@ import static com.taek_aaa.goalsnowball.MainActivity.goalDataSet;
  */
 
 public class TodayGoalDialog extends Dialog implements View.OnClickListener{
-    EditText editText;
+    EditText editTextContents, editTextAmonut;
     String textContents;
     TextView title;
+    RadioGroup radioGroup;
+    Spinner spinner;
+    RadioButton timeRadio, physicalRadio;
+    int checkedId;
+    static final int DEFAULT_RADIOBUTTON_ID = 2131624092;
 
     public TodayGoalDialog(Context context) {
         super(context);
         setContentView(R.layout.dialog_todaygoal);
 
-        editText = (EditText) findViewById(R.id.todayDialogEditText);
+        editTextContents = (EditText) findViewById(R.id.todayDialogEditText);
         findViewById(R.id.todayDialogConfirmButton).setOnClickListener(this);
         findViewById(R.id.todayDialogExitButton).setOnClickListener(this);
         findViewById(R.id.todayDialogX).setOnClickListener(this);
         title = (TextView)findViewById(R.id.title_today_dialog);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup_today);
+        spinner = (Spinner)findViewById(R.id.today_spinner);
+        editTextAmonut = (EditText)findViewById(R.id.setting_amount_today);
+        physicalRadio = (RadioButton)findViewById(R.id.amountOfPhysical_today);
+        timeRadio = (RadioButton)findViewById(R.id.amountOfTime_today);
+        physicalRadio.setChecked(true);
+        checkedId = radioGroup.getCheckedRadioButtonId();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if((radioGroup.getCheckedRadioButtonId())==DEFAULT_RADIOBUTTON_ID){
+
+                    ArrayAdapter adapter = ArrayAdapter.createFromResource(getContext(), R.array.unit,android.R.layout.simple_spinner_item);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter);
+                    editTextAmonut.setHint("양");
+                }else{
+                    ArrayAdapter adapter2 = ArrayAdapter.createFromResource(getContext(),R.array.upAndDown,android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter2);
+                    editTextAmonut.setHint("분");
+                }
+            }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                goalDataSet.setUnitToday(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
 
     }
 
@@ -34,18 +83,39 @@ public class TodayGoalDialog extends Dialog implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.todayDialogConfirmButton:
-                textContents = editText.getText().toString();
+                Log.e("test",""+checkedId);
+                int textAmount = Integer.parseInt(editTextAmonut.getText().toString());
+                textContents = editTextContents.getText().toString();
+
+               if(physicalRadio.isChecked()){
+                   goalDataSet.setTypeToday("물리적양");
+               }else{
+                   goalDataSet.setTodayGoal("시간적양");
+               }
                 /*if(textContents.equals("")){
                     goalDataSet.isTodayGoal=false;
                 }*/
+
+                goalDataSet.setAmountToday(textAmount);
                 goalDataSet.setTodayGoal(textContents);
+
                 Log.e("test",goalDataSet.getTodayGoal());
                 Log.e("test",""+goalDataSet.isTodayGoal);
+
                 if(goalDataSet.isTodayGoal==true){
                     title.setText("오늘의 목표를 수정하세요.");
+                    editTextContents.setHint("목표를 수정하세요.");
                 }else{
                     title.setText("오늘의 목표를 추가하세요.");
+                    editTextContents.setHint("목표를 추가하세요.");
                 }
+                Log.e("data",goalDataSet.getTypeToday());
+                Log.e("data",""+goalDataSet.getAmountToday());
+                Log.e("data",""+goalDataSet.getUnitToday());
+                Log.e("data",goalDataSet.getTodayGoal());
+                Log.e("data",""+goalDataSet.isTodayGoal);
+
+
                 dismiss();
                 break;
             case R.id.todayDialogExitButton:
