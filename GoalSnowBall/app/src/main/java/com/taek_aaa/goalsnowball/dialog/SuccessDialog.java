9a91f8eb5862a8fd8e513484e4911529f4a2b5d2
oh geun taek <rmsxor94@naver.com>
2017-01-18 01:44:06 +0900

    [기능구현] 성공시 wav 파일 재생 구현

--- GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/dialog/SuccessDialog.java
+++ GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/dialog/SuccessDialog.java
@@ -2,6 +2,7 @@ package com.taek_aaa.goalsnowball.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.media.SoundPool;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.taek_aaa.goalsnowball.R;
 
+import static android.media.AudioManager.STREAM_MUSIC;
 import static com.taek_aaa.goalsnowball.activity.MainActivity.goalDataSet;
 
 /**
@@ -24,6 +26,8 @@ public class SuccessDialog extends Dialog {
 
     ImageView fire, coin;
     TextView msg;
+    SoundPool soundPool;
+    int tune;
     public SuccessDialog(Context context) {
         super(context);
         setContentView(R.layout.dialog_success);
@@ -32,7 +36,8 @@ public class SuccessDialog extends Dialog {
         coin = (ImageView)findViewById(R.id.coin);
         msg = (TextView)findViewById(R.id.SuccessCoinMsg);
 
-
+        soundPool = new SoundPool(1,STREAM_MUSIC,0);
+        tune=soundPool.load(getContext(),R.raw.clap,1);
 
         GlideDrawableImageViewTarget imageViewTarget1 = new GlideDrawableImageViewTarget(fire);
         Glide.with(getContext()).load(R.raw.firework2).into(imageViewTarget1);
@@ -44,13 +49,16 @@ public class SuccessDialog extends Dialog {
 
         if(whereSuccess==SUCCESS_FROM_TODAY) {
             msg.setText("" + goalDataSet.getBettingGoldToday() + "Gold를 획득하였습니다.");
+            soundPool.play(tune,1,1,0,0,1);
             goalDataSet.setBettingGoldToday(0);
             whereSuccess=0;
         }else if(whereSuccess==SUCCESS_FROM_WEEK){
+            soundPool.play(tune,1,1,0,0,1);
             msg.setText("" + goalDataSet.getBettingGoldWeek() + "Gold를 획득하였습니다.");
             goalDataSet.setBettingGoldWeek(0);
             whereSuccess=0;
         }else if(whereSuccess==SUCCESS_FROM_MONTH){
+            soundPool.play(tune,1,1,0,0,1);
             msg.setText("" + goalDataSet.getBettingGoldMonth() + "Gold를 획득하였습니다.");
             goalDataSet.setBettingGoldMonth(0);
             whereSuccess=0;
