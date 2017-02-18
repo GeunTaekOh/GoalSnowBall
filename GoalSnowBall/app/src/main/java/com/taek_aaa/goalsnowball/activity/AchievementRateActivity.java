8f827979e743ac46952fd2dc9e1c1f48ec0a132f
oh geun taek <rmsxor94@naver.com>
2017-02-18 21:46:20 +0900

    [UI 대폭 수정]

--- GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/activity/AchievementRateActivity.java
+++ GoalSnowBall/app/src/main/java/com/taek_aaa/goalsnowball/activity/AchievementRateActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -196,6 +197,10 @@ public class AchievementRateActivity extends Activity implements AchievementRate
         animation.setDuration(2500);
         animation.setInterpolator(new DecelerateInterpolator());
         animation.start();
+    }
+
+    public void onClickBackSpace(View v){
+        onBackPressed();
     }
 
 }
